d7f25b4cb80f8c97c08ff93f48b9c58b1b347b48
Ben-Sicat <bensicat00@gmail.com>
2022-06-08 20:06:39 +0800

    started learning python and will try to continue everytning in pyton

---
+++ DOJO/HackerHero/index.py
@@ -0,0 +1,10 @@
+# print("Hellow World")\
+
+
+# def printEven4To22():
+#     sum = 0
+#     for i in range(4,23):
+#         if i % 2 == 0:
+#             print(i)
+#             sum += i    
+#     return sum
