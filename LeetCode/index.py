f56c54a1ea977fd34c1d70a7050b921155b73920
Ben-Sicat <bensicat00@gmail.com>
2022-07-04 17:57:47 +0800

    leetcode 121:

--- LeetCode/index.py
+++ LeetCode/index.py
@@ -87,3 +87,26 @@ class Solution:
             else:
                 j+=1
         return result
+    def  maxProfit(self, prices:list[int]) -> int:
+        # overthinking this problem made it ten times harderr from the start
+        # I think I need to think of a better way to do this, nay a simplier way
+       
+        #You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+        # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+        # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+        l , r =  0, 1
+        maxProfit = 0
+
+        while  r  < len(prices):
+            if prices[r] > prices[l]:
+                profit =  prices[r] - prices[l]
+                print ("priices l", prices[l], "prices r",prices[r])
+                maxProfit= max(maxProfit, profit)
+            else:
+                l = r
+            r+=1
+        return maxProfit
+    print(maxProfit(1,[7,1,5,3,6,4]))
