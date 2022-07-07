c97a55100e8542fe1342ee860f12b3eaad9a234c
Ben-Sicat <bensicat00@gmail.com>
2022-07-07 15:54:07 +0800

    leetcode 74

--- LeetCode/index.py
+++ LeetCode/index.py
@@ -1,3 +1,6 @@
+from re import L
+
+
 class Solution:
     # def containsDuplicate(self, nums: List[int]) -> bool:
     #     return len(nums) != len(set(nums))
@@ -123,3 +126,29 @@ class Solution:
             for j in range(len(mat[0])):
                 newMat[i][j] = mat[i][j]
         return newMat
+
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        #either recursive or iterative binary serach:
+        ROWS, COLS =  len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot ) // 2
+            if target > matrix[row][-1]:
+                top = row +1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not ( top <= bot):
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r:
+            m = (l + r) // 2
+            if target  > matrix [row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+            return False
