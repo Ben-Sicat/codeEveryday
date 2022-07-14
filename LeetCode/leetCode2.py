1936493a53cb840dfe33717e6f0e545383aaf945
Ben-Sicat <bensicat00@gmail.com>
2022-07-14 21:55:13 +0800

    eh

--- LeetCode/leetCode2.py
+++ LeetCode/leetCode2.py
@@ -51,4 +51,8 @@ class Solution:
                 row.append(temp[j]+temp[j+1])
             res.append(row)
         return res
+
+        def random_function(self):
+            self.array = {}
+            
         # make use of the logic of pointers
