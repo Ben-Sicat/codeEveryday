174f37dd9023615c98a4da6b05acbbe6ee1d77f8
Ben-Sicat <bensicat00@gmail.com>
2022-07-22 00:48:29 +0800

    ehe

--- projects/RPS/index.js
+++ projects/RPS/index.js
@@ -1,2 +1,7 @@
 const elementR = document.getElementById('user_button_rock');
 const elementP = document.getElementById('user_button_paper');
+const elementS = document.getElementById('user_button_scissors;');
+
+x = () =>{
+    let = "idk yet i forgot maybe later"
+}
