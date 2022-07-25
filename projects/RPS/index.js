4b9603dcee0c144dc090b5a4eee70d589937a840
Ben-Sicat <bensicat00@gmail.com>
2022-07-25 18:18:39 +0800

    ehe

--- projects/RPS/index.js
+++ projects/RPS/index.js
@@ -2,6 +2,6 @@ const elementR = document.getElementById('user_button_rock');
 const elementP = document.getElementById('user_button_paper');
 const elementS = document.getElementById('user_button_scissors;');
 
-x = () =>{
-    let = "idk yet i forgot maybe later"
-}
+const element_computer = ["ROCK", "PAPER", "SCISSORS"];
+
+const computer_choice = Math.floor(Math.random() * 3);
