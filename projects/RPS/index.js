79f8ae91d4a881b814027083436d77e551abc39a
Ben-Sicat <bensicat00@gmail.com>
2022-07-25 18:24:01 +0800

    ehe

--- projects/RPS/index.js
+++ projects/RPS/index.js
@@ -5,3 +5,10 @@ const elementS = document.getElementById('user_button_scissors;');
 const element_computer = ["ROCK", "PAPER", "SCISSORS"];
 
 const computer_choice = Math.floor(Math.random() * 3);
+
+
+elementR.addEventListener('click', )
+
+function button_behaviour(){
+    // something here to make it do something
+}
