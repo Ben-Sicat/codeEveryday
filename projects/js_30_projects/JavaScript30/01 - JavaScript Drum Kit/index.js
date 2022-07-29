bad87ee1011e8953432418a25fc91606e1ab0db3
Ben-Sicat <bensicat00@gmail.com>
2022-07-30 04:37:31 +0800

    drums

--- projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
+++ projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -1,11 +1,24 @@
 window.addEventListener('keydown',function(e){
     console.log(e.keyCode)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     console.log(audio)
 
     if(!audio) return;
+    audio.currentTime = 0;
     audio.play();
 
+    key.classList.add('playing')
     // next step is to target classnames to change the stylying on keydown
-    
+
+   
+
+    //gotta create a function to remove the transition
+
 });
+function removeTransition(e){
+    console.log(e)
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
