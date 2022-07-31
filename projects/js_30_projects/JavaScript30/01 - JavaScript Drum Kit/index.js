eb0ab4c547283ed49e99930af6b4e629bb47b8d7
Ben-Sicat <bensicat00@gmail.com>
2022-07-31 16:19:47 +0800

    drum

--- projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
+++ projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -18,8 +18,10 @@ window.addEventListener('keydown',function(e){
 
 });
 function removeTransition(e){
-    console.log(e)
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing')
 }
+
 
 const keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));
