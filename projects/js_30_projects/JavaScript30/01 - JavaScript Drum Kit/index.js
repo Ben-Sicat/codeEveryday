f968dc0904c38ee04cfbb2e575d6b1c3cf00e5cc
Ben-Sicat <bensicat00@gmail.com>
2022-08-01 05:18:31 +0800

    ehe

--- projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
+++ projects/js_30_projects/JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -1,27 +1,48 @@
-window.addEventListener('keydown',function(e){
-    console.log(e.keyCode)
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    console.log(audio)
+// window.addEventListener('keydown',function(e){
 
-    if(!audio) return;
-    audio.currentTime = 0;
+
+//     console.log(e.keyCode)
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+//     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+//     console.log(audio)
+
+//     if(!audio) return;
+//     audio.currentTime = 0;
+//     audio.play();
+
+//     key.classList.add('playing')
+//     // next step is to target classnames to change the stylying on keydown
+
+//    console.log(key.classList)
+
+//     //gotta create a function to remove the transition
+
+// });
+// function removeTransition(e){
+//     if(e.propertyName !== 'transform') return;
+//     this.classList.remove('playing')
+// }
+
+
+// const keys = document.querySelectorAll('.key')
+// keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+window.addEventListener('keydown',function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+
+    if (!audio) return;
+
+    audio.currentTime= 0;
     audio.play();
 
     key.classList.add('playing')
     
-    // next step is to target classnames to change the stylying on keydown
+})
 
-   console.log(key.classList)
-
-    //gotta create a function to remove the transition
-
-});
 function removeTransition(e){
-    if(e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing')
+    if(e.propertyName !== 'transform')return;
+    this.classList.remove('playing')
 }
 
-
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
